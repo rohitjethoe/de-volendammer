d23dab7e525e70d2b73d9eb79faf7fc55fc8018b
rohitjethoe <rohitchai@icloud.com>
2022-07-15 19:53:59 +0200

    Changed 404 page content.

--- 404.php
+++ 404.php
@@ -7,50 +7,22 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'de-volendammer' ); ?></h1>
-			</header><!-- .page-header -->
-
+		<div class="error-404 not-found">
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'de-volendammer' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'de-volendammer' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$de_volendammer_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'de-volendammer' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$de_volendammer_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+				<div class="error">
+					<div class="error-header">
+						Oeps...
+					</div>
+					<div class="error-content">
+						404 - Pagina niet gevonden.
+					</div>
+					<div class="error-return">
+						Wil je terug naar de home pagina?
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
 
 <?php
 get_footer();
